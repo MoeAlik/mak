0fc5a1d3b5b201031f06f9e93b92639505eaa7e3
Mohammadali Kodeih <qomq@Mohammadalis-MacBook-Pro.local>
2025-05-18 12:20:48 -0700

    added size_t, working on swap

--- headers/utility.h
+++ headers/utility.h
@@ -2,8 +2,19 @@
 #include "type_traits.h"
 
 namespace mak {
+using size_t = decltype(sizeof 0);
+
 template <class T> constexpr remove_reference_t<T> &&move(T &&t) noexcept {
   return static_cast<remove_reference_t<T> &&>(t);
 }
-void swap(auto &lhs, auto &rhs);
+
+template <class T> T exchange(T &obj, T &&new_value) {
+  T old = obj;
+  obj = new_value;
+  return old;
+}
+
+void swap(auto &lhs, auto &rhs) { lhs = exchange(rhs); }
+
+template <class T, size_t N> swap(T (&a)[N], T (&b)[N]) {}
 } // namespace mak
