b97e2251f981581a5420a62586c14acb61b1cc56
Mohammadali Kodeih <qomq@Mohammadalis-MacBook-Pro.local>
2025-05-31 21:21:00 -0700

    added pair constructors

--- headers/utility.h
+++ headers/utility.h
@@ -8,12 +8,16 @@ template <class T> constexpr remove_reference_t<T> &&move(T &&t) noexcept {
 }
 
 template <class T, class U = T> T exchange(T &obj, U &&new_value) noexcept {
-  T old = obj;
-  obj = new_value;
+  T old = move(obj);
+  obj = move(new_value);
   return old;
 }
 
-void swap(auto &lhs, auto &rhs) noexcept { rhs = exchange(lhs, rhs); }
+void swap(auto &lhs, auto &rhs) noexcept {
+  auto old = move(lhs);
+  lhs = move(rhs);
+  rhs = move(old);
+}
 
 template <class T, size_t N> void swap(T (&a)[N], T (&b)[N]) noexcept {
   for (size_t i{}; i < N; ++i)
@@ -25,8 +29,33 @@ template <class T> constexpr T &&forward(remove_reference_t<T> &t) noexcept {
 }
 
 template <class T> constexpr T &&forward(remove_reference_t<T> &&t) noexcept {
-  // TODO: why do we need static_assert
   return static_cast<T &&>(t);
 }
+
+template <class T1, class T2> struct pair {
+  T1 first;
+  T2 second;
+
+  constexpr pair() : first{}, second{} {}
+  constexpr pair(const T1 &x, const T2 &y) : first{x}, second{y} {}
+
+  template <class U1 = T1, class U2 = T2>
+  constexpr pair(U1 &&x, U2 &&y) : first{forward(x)}, second{forward(y)} {}
+  template <class U1, class U2>
+  constexpr pair(pair<U1, U2> &p) : first{p.first}, second{p.second} {}
+
+  template <class U1, class U2>
+  pair(const pair<U1, U2> &p) : first{p.first}, second{p.second} {}
+
+  template <class U1, class U2>
+  pair(pair<U1, U2> &&p) : first{forward(p.first)}, second{forward(p.second)} {}
+
+  template <class U1, class U2>
+  constexpr pair(const pair<U1, U2> &&p)
+      : first{forward(p.first)}, second{forward(p.second)} {}
+
+  pair(const pair &p) = default;
+  pair(pair &&p) = default;
+};
 
 } // namespace mak
--- notes/todo.md
+++ notes/todo.md
@@ -1,3 +1,4 @@
 # To do
 
 - condtionally noexcept
+- static assert to see if t is an lvalue reference. This involves implementing is_lvalue_reference which involves implementing integral_constant
