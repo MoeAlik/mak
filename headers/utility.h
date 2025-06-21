ad554d8680bebad92dc5e7543927190370cc27d1
Mohammadali Kodeih <qomq@Mohammadalis-MacBook-Pro.local>
2025-06-20 20:23:55 -0400

    working on test suite helpers

--- headers/utility.h
+++ headers/utility.h
@@ -2,70 +2,66 @@
 #include "type_traits.h"
 
 namespace mak {
-using size_t = decltype(sizeof(const volatile unsigned long long int* const* const));
-template <class T>
-constexpr remove_reference_t<T>&& move(T&& t) noexcept {
-    return static_cast<mak::remove_reference_t<T>&&>(t);
+using size_t =
+    decltype(sizeof(const volatile unsigned long long int *const *const));
+template <class T> constexpr remove_reference_t<T> &&move(T &&t) noexcept {
+  return static_cast<mak::remove_reference_t<T> &&>(t);
 }
 
-template <class T, class U = T>
-T exchange(T& obj, U&& new_value) noexcept {
-    T old = mak::move(obj);
-    obj = mak::move(new_value);
-    return old;
+template <class T, class U = T> T exchange(T &obj, U &&new_value) noexcept {
+  T old = mak::move(obj);
+  obj = mak::move(new_value);
+  return old;
 }
 
-template <class T>
-void swap(T& lhs, T& rhs) noexcept {
-    T old = mak::move(lhs);
-    lhs = mak::move(rhs);
-    rhs = mak::move(old);
+template <class T> void swap(T &lhs, T &rhs) noexcept {
+  T old = mak::move(lhs);
+  lhs = mak::move(rhs);
+  rhs = mak::move(old);
 }
 
-template <class T, size_t N>
-void swap(T (&a)[N], T (&b)[N]) noexcept {
-    for (size_t i {}; i < N; ++i) {
-        mak::swap(a[i], b[i]);
-    }
+template <class T, size_t N> void swap(T (&a)[N], T (&b)[N]) noexcept {
+  for (size_t i{}; i < N; ++i) {
+    mak::swap(a[i], b[i]);
+  }
 }
 
-template <class T>
-constexpr T&& forward(remove_reference_t<T>& t) noexcept {
-    return static_cast<T&&>(t);
+template <class T> constexpr T &&forward(remove_reference_t<T> &t) noexcept {
+  return static_cast<T &&>(t);
 }
 
-template <class T>
-constexpr T&& forward(remove_reference_t<T>&& t) noexcept {
-    return static_cast<T&&>(t);
+template <class T> constexpr T &&forward(remove_reference_t<T> &&t) noexcept {
+  return static_cast<T &&>(t);
 }
 
-// template <class T1, class T2> struct pair {
-//   T1 first;
-//   T2 second;
+template <class T1, class T2> struct pair {
+  T1 first;
+  T2 second;
 
-//   constexpr pair() : first{}, second{} {}
-//   constexpr pair(const T1 &x, const T2 &y) : first{x}, second{y} {}
+  constexpr pair() : first{}, second{} {}
+  //   constexpr pair(const T1 &x, const T2 &y) : first{x}, second{y} {}
 
-//   template <class U1 = T1, class U2 = T2>
-//   constexpr pair(U1 &&x, U2 &&y) : first{forward(x)}, second{forward(y)} {}
-//   template <class U1, class U2>
-//   constexpr pair(pair<U1, U2> &p) : first{p.first}, second{p.second} {}
+  //   template <class U1 = T1, class U2 = T2>
+  //   constexpr pair(U1 &&x, U2 &&y) : first{forward(x)}, second{forward(y)} {}
+  //   template <class U1, class U2>
+  //   constexpr pair(pair<U1, U2> &p) : first{p.first}, second{p.second} {}
 
-//   template <class U1, class U2>
-//   pair(const pair<U1, U2> &p) : first{p.first}, second{p.second} {}
+  //   template <class U1, class U2>
+  //   pair(const pair<U1, U2> &p) : first{p.first}, second{p.second} {}
 
-//   template <class U1, class U2>
-//   pair(pair<U1, U2> &&p) : first{forward(p.first)}, second{forward(p.second)}
-//   {}
+  //   template <class U1, class U2>
+  //   pair(pair<U1, U2> &&p) : first{forward(p.first)},
+  //   second{forward(p.second)}
+  //   {}
 
-//   template <class U1, class U2>
-//   constexpr pair(const pair<U1, U2> &&p)
-//       : first{forward(p.first)}, second{forward(p.second)} {}
+  //   template <class U1, class U2>
+  //   constexpr pair(const pair<U1, U2> &&p)
+  //       : first{forward(p.first)}, second{forward(p.second)} {}
 
-//   pair(const pair &p) = default;
-//   pair(pair &&p) = default;
+  //   pair(const pair &p) = default;
+  //   pair(pair &&p) = default;
 
-// pair &operator=(const pair& other);
-// };
+  // pair &operator=(const pair& other);
+};
 
-}   // namespace mak
+} // namespace mak
--- test/utility_test.cpp
+++ test/utility_test.cpp
@@ -1,6 +1,7 @@
 #include "headers/utility.h"
 
 #include <array>
+#include <memory>
 #include <stdexcept>
 #include <string>
 
@@ -9,84 +10,96 @@
 namespace {
 
 struct move_only_t {
-    move_only_t() = delete;
-    move_only_t(const move_only_t&) = delete;
-    move_only_t& operator=(const move_only_t&) = delete;
-    move_only_t& operator=(move_only_t&&) noexcept { return *this; }
-    move_only_t(int&&) noexcept {}
+  move_only_t() = delete;
+  move_only_t(const move_only_t &) = delete;
+  move_only_t &operator=(const move_only_t &) = delete;
+  move_only_t &operator=(move_only_t &&) noexcept { return *this; }
+  move_only_t(int &&) noexcept {}
 };
 
-
 void test_move() {
-    int dummy {};
-    move_only_t move_only(mak::move(dummy));
-    move_only = move_only_t(mak::move(1));
+  int dummy{};
+  move_only_t move_only(mak::move(dummy));
+  move_only = move_only_t(mak::move(1));
 
-    int& dummy_ref = dummy;
-    move_only = move_only_t(mak::move(dummy_ref));
+  int &dummy_ref = dummy;
+  move_only = move_only_t(mak::move(dummy_ref));
 }
 
 void test_exchange() {
-    int a = 1;
-    int b = mak::exchange(a, 2);
-    if (a != 2) {
-        throw std::runtime_error("exchange failed to modify value of obj");
-    }
-    if (b != 1) {
-        throw std::runtime_error("exchange failed to return original value of obj");
-    }
+  int a = 1;
+  int b = mak::exchange(a, 2);
+  if (a != 2) {
+    throw std::runtime_error("exchange failed to modify value of obj");
+  }
+  if (b != 1) {
+    throw std::runtime_error("exchange failed to return original value of obj");
+  }
 }
 
 void test_swaps() {
-    int a = 1, b = 2;
-    mak::swap(a, b);
-    if (a != 2 or b != 1) {
-        throw std::runtime_error("swap failed");
+  int a = 1, b = 2;
+  mak::swap(a, b);
+  if (a != 2 or b != 1) {
+    throw std::runtime_error("swap failed");
+  }
+
+  int lhs[17] = {0};
+  int rhs[17] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+  mak::swap(lhs, rhs);
+
+  for (mak::size_t i{}; i < 17; ++i) {
+    if (lhs[i] != 1 or rhs[i] != 0) {
+      throw std::runtime_error("swapping array failed on item " +
+                               std::to_string(i));
     }
-
-    int lhs[17] = { 0 };
-    int rhs[17] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-    mak::swap(lhs, rhs);
-
-    for (mak::size_t i {}; i < 17; ++i) {
-        if (lhs[i] != 1 or rhs[i] != 0) {
-            throw std::runtime_error("swapping array failed on item " + std::to_string(i));
-        }
-    }
+  }
 }
 
-//
-int forward_overload(int&) {
-    return 1;
-}
+int forward_overload(int &) { return 1; }
 
-float forward_overload(int&&) {
-    return 0;
-}
+float forward_overload(int &&) { return 0; }
 
-template <typename T>
-int forward_test(T&& val) {
-    return forward_overload(mak::forward<T>(val));
+template <typename T> int forward_test(T &&val) {
+  return forward_overload(mak::forward<T>(val));
 }
 
 void test_forward() {
-    int lval {};
-    int a = forward_test(lval);
-    if (a != 1) {
-        throw std::runtime_error("Forward failed to forward lvalue as lvalue");
-    }
+  int lval{};
+  int a = forward_test(lval);
+  if (a != 1) {
+    throw std::runtime_error("Forward failed to forward lvalue as lvalue");
+  }
 
-    a = forward_test(mak::move(lval));
-    if (a != 0) {
-        throw std::runtime_error("Forward failed to forward rvalue as rvalue");
-    }
+  a = forward_test(mak::move(lval));
+  if (a != 0) {
+    throw std::runtime_error("Forward failed to forward rvalue as rvalue");
+  }
 }
 
-}   // namespace
+void test_pain_constructor() {
+  mak::pair<long, double> default_init;
+  if constexpr (!std::is_same_v<decltype(default_init.first), long>) {
+  }
+  if constexpr (!std::is_same_v<decltype(default_init.second), double>) {
+  }
+  if (default_init.first != 0 || default_init.second != 0.0) {
+  }
+  /*[[maybe_unused]]*/ mak::pair<long, double> value_init{};
+  if constexpr (!std::is_same_v<decltype(value_init.first), long>) {
+  }
+  if constexpr (!std::is_same_v<decltype(value_init.second), double>) {
+  }
+  if (value_init.first != 0 || value_init.second != 0.0) {
+  }
+}
+
+} // namespace
 
 void test_utility() {
-    test_move();
-    test_exchange();
-    test_swaps();
-    test_forward();
+  test_move();
+  test_exchange();
+  test_swaps();
+  test_forward();
+  test_pain_constructor();
 }
