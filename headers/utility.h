e14733758e256857a3e76627e3a9fbdf402e6626
MoeAlik <moegame1123@gmail.com>
2025-07-05 18:35:41 -0400

    testing std::get<>

--- headers/utility.h
+++ headers/utility.h
@@ -1,101 +1,117 @@
 #pragma once
-#include "type_traits.h"
 #include <type_traits>
 
+#include "type_traits.h"
+
 namespace mak {
-static inline constexpr const volatile unsigned long long int *const &entity{};
+static inline constexpr const volatile unsigned long long int* const& entity {};
 using size_t = decltype(sizeof(entity));
-template <class T> constexpr remove_reference_t<T> &&move(T &&t) noexcept {
-  return static_cast<mak::remove_reference_t<T> &&>(t);
+template <class T>
+constexpr remove_reference_t<T>&& move(T&& t) noexcept {
+    return static_cast<mak::remove_reference_t<T>&&>(t);
 }
 
-template <class T, class U = T> T exchange(T &obj, U &&new_value) noexcept {
-  T old = mak::move(obj);
-  obj = mak::move(new_value);
-  return old;
+template <class T, class U = T>
+T exchange(T& obj, U&& new_value) noexcept {
+    T old = mak::move(obj);
+    obj = mak::move(new_value);
+    return old;
 }
 
-template <class T> void swap(T &lhs, T &rhs) noexcept {
-  T old = mak::move(lhs);
-  lhs = mak::move(rhs);
-  rhs = mak::move(old);
+template <class T>
+void swap(T& lhs, T& rhs) noexcept {
+    T old = mak::move(lhs);
+    lhs = mak::move(rhs);
+    rhs = mak::move(old);
 }
 
-template <class T, size_t N> void swap(T (&a)[N], T (&b)[N]) noexcept {
-  for (size_t i{}; i < N; ++i) {
-    mak::swap(a[i], b[i]);
-  }
+template <class T, size_t N>
+void swap(T (&a)[N], T (&b)[N]) noexcept {
+    for (size_t i {}; i < N; ++i) {
+        mak::swap(a[i], b[i]);
+    }
 }
 
-template <class T> constexpr T &&forward(remove_reference_t<T> &t) noexcept {
-  return static_cast<T &&>(t);
+template <class T>
+constexpr T&& forward(remove_reference_t<T>& t) noexcept {
+    return static_cast<T&&>(t);
 }
 
-template <class T> constexpr T &&forward(remove_reference_t<T> &&t) noexcept {
-  return static_cast<T &&>(t);
-}
-
-template <class T1, class T2> struct pair {
-  T1 first;
-  T2 second;
-
-  constexpr pair() : first{}, second{} {}
-  constexpr pair(const T1 &x, const T2 &y) : first{x}, second{y} {}
-
-  template <class U1 = T1, class U2 = T2>
-  constexpr pair(U1 &&x, U2 &&y)
-      : first{mak::forward<U1>(x)}, second{mak::forward<U2>(y)} {}
-
-  template <class U1, class U2>
-  pair(const pair<U1, U2> &p) : first{p.first}, second{p.second} {}
-
-  template <class U1, class U2>
-  constexpr pair(const pair<U1, U2> &&p)
-      : first{mak::forward<U1>(p.first)}, second{mak::forward<U2>(p.second)} {}
-
-  //  TODO: get, pair asssigment, <=>, ==
-  void swap(pair<T1, T2> &other) noexcept { mak::swap(other, *this); }
-};
-
-template <size_t I, class T> struct tuple_element;
-
-template <size_t I, class T1, class T2> struct tuple_element<I, pair<T1, T2>> {
-  tuple_element() { throw "out of bounds"; }
-};
-
-template <class T1, class T2> struct tuple_element<0, pair<T1, T2>> {
-  using type = T1;
-};
-
-template <class T1, class T2> struct tuple_element<1, pair<T1, T2>> {
-  using type = T2;
-};
-template <size_t I, typename T1, typename T2>
-const typename tuple_element<I, pair<T1, T2>>::type &
-get(const pair<T1, T2> &p) {
-  static_assert(p);
-}
-
-template <typename T1, typename T2>
-const typename tuple_element<0, pair<T1, T2>>::type &
-get(const pair<T1, T2> &p) {
-  return p.first;
-}
-
-template <size_t I, typename T1, typename T2>
-const typename tuple_element<I, pair<T1, T2>>::type &&
-get(const pair<T1, T2> &&p) {
-  if constexpr (std::is_same_v<typename tuple_element<I, pair<T1, T2>>::type,
-                               T1>) {
-    return p.first;
-  } else {
-    return p.second;
-  }
+template <class T>
+constexpr T&& forward(remove_reference_t<T>&& t) noexcept {
+    return static_cast<T&&>(t);
 }
 
 template <class T1, class T2>
-constexpr mak::pair<T1, T2> make_pair(T1 &&x, T2 &&y) {
-  return mak::pair<T1, T2>{mak::forward<T1>(x), mak::forward<T2>(y)};
+struct pair {
+    T1 first;
+    T2 second;
+
+    constexpr pair()
+        : first {}
+        , second {} {}
+    constexpr pair(const T1& x, const T2& y)
+        : first { x }
+        , second { y } {}
+
+    template <class U1 = T1, class U2 = T2>
+    constexpr pair(U1&& x, U2&& y)
+        : first { mak::forward<U1>(x) }
+        , second { mak::forward<U2>(y) } {}
+
+    template <class U1, class U2>
+    pair(const pair<U1, U2>& p)
+        : first { p.first }
+        , second { p.second } {}
+
+    template <class U1, class U2>
+    constexpr pair(const pair<U1, U2>&& p)
+        : first { mak::forward<U1>(p.first) }
+        , second { mak::forward<U2>(p.second) } {}
+
+    //  TODO: get, pair asssigment, <=>, ==
+    void swap(pair<T1, T2>& other) noexcept { mak::swap(other, *this); }
+};
+
+template <size_t I, class T>
+struct tuple_element;
+
+template <size_t I, class T1, class T2>
+struct tuple_element<I, pair<T1, T2>> {
+    tuple_element() { throw "out of bounds"; }
+};
+
+template <class T1, class T2>
+struct tuple_element<0, pair<T1, T2>> {
+    using type = T1;
+};
+
+template <class T1, class T2>
+struct tuple_element<1, pair<T1, T2>> {
+    using type = T2;
+};
+template <size_t I, typename T1, typename T2>
+const typename tuple_element<I, pair<T1, T2>>::type& get(const pair<T1, T2>&) {
+    throw "wijhak";
 }
 
-} // namespace mak
+template <typename T1, typename T2>
+const typename tuple_element<0, pair<T1, T2>>::type& get(const pair<T1, T2>& p) {
+    return p.first;
+}
+
+template <size_t I, typename T1, typename T2>
+const typename tuple_element<I, pair<T1, T2>>::type&& get(const pair<T1, T2>&& p) {
+    if constexpr (std::is_same_v<typename tuple_element<I, pair<T1, T2>>::type, T1>) {
+        return p.first;
+    } else {
+        return p.second;
+    }
+}
+
+template <class T1, class T2>
+constexpr mak::pair<T1, T2> make_pair(T1&& x, T2&& y) {
+    return mak::pair<T1, T2> { mak::forward<T1>(x), mak::forward<T2>(y) };
+}
+
+}   // namespace mak
--- test/utility_test.cpp
+++ test/utility_test.cpp
@@ -13,124 +13,128 @@ using test::assert_not;
 namespace {
 
 struct move_only_t {
-  move_only_t() = delete;
-  move_only_t(const move_only_t &) = delete;
-  move_only_t &operator=(const move_only_t &) = delete;
-  move_only_t &operator=(move_only_t &&) noexcept { return *this; }
-  move_only_t(int &&) noexcept {}
+    move_only_t() = delete;
+    move_only_t(const move_only_t&) = delete;
+    move_only_t& operator=(const move_only_t&) = delete;
+    move_only_t& operator=(move_only_t&&) noexcept { return *this; }
+    move_only_t(int&&) noexcept {}
 };
 
 void move() {
-  int dummy{};
-  move_only_t move_only(mak::move(dummy));
-  move_only = move_only_t(mak::move(1));
+    int dummy {};
+    move_only_t move_only(mak::move(dummy));
+    move_only = move_only_t(mak::move(1));
 
-  int &dummy_ref = dummy;
-  move_only = move_only_t(mak::move(dummy_ref));
+    int& dummy_ref = dummy;
+    move_only = move_only_t(mak::move(dummy_ref));
 }
 
 void exchange() {
-  int a = 1;
-  int b = mak::exchange(a, 2);
-  assert(a == 2, "exchange failed to modify value of obj");
-  assert(b == 1, "exchange failed to return original value of obj");
+    int a = 1;
+    int b = mak::exchange(a, 2);
+    assert(a == 2, "exchange failed to modify value of obj");
+    assert(b == 1, "exchange failed to return original value of obj");
 }
 
 void swaps() {
-  int a = 1, b = 2;
-  mak::swap(a, b);
-  assert(a == 2 and b == 1, "swap failed");
+    int a = 1, b = 2;
+    mak::swap(a, b);
+    assert(a == 2 and b == 1, "swap failed");
 
-  int lhs[17] = {0};
-  int rhs[17] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-  mak::swap(lhs, rhs);
+    int lhs[17] = { 0 };
+    int rhs[17] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+    mak::swap(lhs, rhs);
 
-  for (mak::size_t i{}; i < 17; ++i) {
-    assert(lhs[i] == 1 and rhs[i] == 0, "swapping array failed on item");
-  }
+    for (mak::size_t i {}; i < 17; ++i) {
+        assert(lhs[i] == 1 and rhs[i] == 0, "swapping array failed on item");
+    }
 }
 
-int forward_overload(int &) { return 1; }
+int forward_overload(int&) {
+    return 1;
+}
 
-float forward_overload(int &&) { return 0; }
+float forward_overload(int&&) {
+    return 0;
+}
 
-template <typename T> int forward_test(T &&val) {
-  return forward_overload(mak::forward<T>(val));
+template <typename T>
+int forward_test(T&& val) {
+    return forward_overload(mak::forward<T>(val));
 }
 
 void forward() {
-  int lval{};
-  int a = forward_test(lval);
-  assert(a == 1, "Forward failed to forward lvalue as lvalue");
+    int lval {};
+    int a = forward_test(lval);
+    assert(a == 1, "Forward failed to forward lvalue as lvalue");
 
-  a = forward_test(mak::move(lval));
-  assert(a == 0, "Forward failed to forward rvalue as rvalue");
+    a = forward_test(mak::move(lval));
+    assert(a == 0, "Forward failed to forward rvalue as rvalue");
 }
 
 void pair_constructor() {
-  mak::pair<long, double> default_init;
-  assert(std::is_same_v<decltype(default_init.first), long>,
-         "Default first is wrong type");
-  assert(std::is_same_v<decltype(default_init.second), double>,
-         "Default second is wrong type");
-  assert(default_init.first == 0, "Default first has wrong value");
-  assert(default_init.second == 0.0, "Default second has wrong value");
+    mak::pair<long, double> default_init;
+    assert(std::is_same_v<decltype(default_init.first), long>, "Default first is wrong type");
+    assert(std::is_same_v<decltype(default_init.second), double>, "Default second is wrong type");
+    assert(default_init.first == 0, "Default first has wrong value");
+    assert(default_init.second == 0.0, "Default second has wrong value");
 
-  mak::pair<long, double> value_init{};
-  assert(value_init.first == 0, "Value first has wrong value");
-  assert(value_init.second == 0.0, "Value second has wrong value");
+    mak::pair<long, double> value_init {};
+    assert(value_init.first == 0, "Value first has wrong value");
+    assert(value_init.second == 0.0, "Value second has wrong value");
 
-  long first = 1;
-  double second = 2.0;
+    long first = 1;
+    double second = 2.0;
 
-  auto func = [&](auto pair) {
-    assert(pair.first == 1, "const ref first has wrong value");
-    assert(pair.second == 2.0, "const ref second has wrong value");
-  };
+    auto func = [&](auto pair) {
+        assert(pair.first == 1, "const ref first has wrong value");
+        assert(pair.second == 2.0, "const ref second has wrong value");
+    };
 
-  std::array<mak::pair<long, double>, 4> const_ref_pairs = {
-      {{1, 2.0}, {1, second}, {first, 2.0}, {first, second}}};
+    std::array<mak::pair<long, double>, 4> const_ref_pairs = {
+        {{ 1, 2.0 }, { 1, second }, { first, 2.0 }, { first, second }}
+    };
 
-  for (const auto &p : const_ref_pairs) {
-    func(p);
-  }
+    for (const auto& p : const_ref_pairs) {
+        func(p);
+    }
 
-  [[maybe_unused]] auto const_ref_pair_input{mak::make_pair(1, 1ll)};
+    [[maybe_unused]] auto const_ref_pair_input { mak::make_pair(1, 1ll) };
 
-  [[maybe_unused]] auto forwarding_l_value_pair(const_ref_pair_input);
+    [[maybe_unused]] auto forwarding_l_value_pair(const_ref_pair_input);
 }
 
 void pair_swap() {
-  auto p1 = mak::make_pair(5.f, 7ull);
-  auto p2 = mak::make_pair(3.f, 2ull);
+    auto p1 = mak::make_pair(5.f, 7ull);
+    auto p2 = mak::make_pair(3.f, 2ull);
 
-  p1.swap(p2);
+    p1.swap(p2);
 
-  assert(p1.first == 3.f, "first first wrong");
-  assert(p1.second == 2ull, "first second wrong");
-  assert(p2.first == 5.f, "second first wrong");
-  assert(p2.second == 7ull, "second second wrong");
+    assert(p1.first == 3.f, "first first wrong");
+    assert(p1.second == 2ull, "first second wrong");
+    assert(p2.first == 5.f, "second first wrong");
+    assert(p2.second == 7ull, "second second wrong");
 }
 
 void pair_get() {
-  auto p1 = mak::make_pair(5.f, 7ull);
-  auto first = mak::get<0>(p1);
-  auto second = mak::get<1>(p1);
-  assert(std::is_same_v<decltype(first), float>, "get 0 wrong");
-  assert(std::is_same_v<decltype(second), size_t>, "get 1 wrong");
+    auto p1 = mak::make_pair(5.f, 7ull);
+    auto first = mak::get<0>(p1);
+    auto second = mak::get<1>(p1);
+    assert(std::is_same_v<decltype(first), float>, "get 0 wrong");
+    assert(std::is_same_v<decltype(second), size_t>, "get 1 wrong");
 
-  // first = mak::get<float>(p1);
-  // second = mak::get<size_t>(p1);
+    // first = mak::get<float>(p1);
+    // second = mak::get<size_t>(p1);
 }
 
-} // namespace
+}   // namespace
 
 void test::utility() {
-  move();
-  exchange();
-  swaps();
-  forward();
-  pair_constructor();
-  pair_swap();
-  pair_get();
+    move();
+    exchange();
+    swaps();
+    forward();
+    pair_constructor();
+    pair_swap();
+    pair_get();
 }
